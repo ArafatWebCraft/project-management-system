ce954e7f553fe475e37d38de4e88165e6d2aee86
ArafatWebCraft <arafathossan377@gmail.com>
2024-02-20 23:14:27 +0600

    simple project

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -7,14 +7,12 @@ use Illuminate\Http\Request;
 use App\Models\Project;
 use App\Models\Task;
 use App\Models\Member;
+use Illuminate\Support\Facades\DB;
 
 class ShowController extends Controller
 {
     public function show_details($id)
     {
-        $projects = Project::with('tasks')->findOrFail($id);
-        $members = Project::where('id', $id)->with('members')->firstOrFail();
-        dd($projects->taks);
-        //return view('details.details', compact('projects', 'members'));
+        $projects = DB::table()
     }
 }
